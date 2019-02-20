b891503ed6582e0e9377ef08b4287b31a527b0a2
Erin Toth <erintoth@gmail.com>
2019-02-20 00:09:04 +0000

    Finish exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -3,4 +3,14 @@ class Store < ActiveRecord::Base
 
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+
+  before_destroy :check_for_employees
+
+  private
+
+    def check_for_employees
+      if employees.any?
+        throw :abort
+      end
+    end
 end
